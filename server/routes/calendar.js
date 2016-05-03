0a0d9b6aa78901c6aa0621c3f3a1cdb29cc0425d
sasham43 <sashasemail@gmail.com>
2016-05-03 13:23:18 -0500

    Google Calendar working now using service account

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,61 +1,54 @@
 var router = require('express').Router();
-var ajax = require('request');
-var gcal = require('google-calendar');
-var moment = require('moment');
 var mongoose = require('mongoose');
+
+var google = require('googleapis');
+var moment = require('moment');
+var key = require('../../dashboard_private_key.json');
 var Event = require('../../models/calendarModel');
+var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, 'https://www.googleapis.com/auth/calendar', null);
+var calendar = google.calendar('v3');
+
+var today = moment().format();
+var tomorrow = moment().add(1, 'day').format();
+
 
 router.get('/', function(req, res){
-  // if no access token, redirect to authentication page
-  // if(!req.session.access_token){
-  //   console.log('not authenticated, redirecting', req.session);
-  //    return res.redirect('/auth');
-  // }
-  //
-  // // create an instance from accessToken
-  // var accessToken = req.session.access_token;
-  // console.log('accessToken:', accessToken);
-
-  var today = moment().format();
-  var tomorrow = moment().add(1, 'day').format();
-
-  gcal('ya29.CjLXAgQGkCIwBCR6dgF0nGDiwdt2TSYs3yutB5nmXMAg7giX3nFzdDaLQ8UKSfz_el3Q8w').events.list('primary', {timeMin: today, timeMax: tomorrow}, function(err, eventList) {
-    if (err){
-      console.log('err getting events', err);
-    } else {
-      console.log('got events:', eventList);
-      var events = eventList.items;
-      var trimmedEvents = [];
-      // loop through and store items in database
-      events.map(function(event){
-        var title = event.summary;
-        var start = event.start.dateTime;
-        var end = event.end.dateTime;
-        var trimmedEvent = {title: title, start: start, end: end};
-        Event.create(trimmedEvent, function(err){
-          if (err){
-            console.log('Error saving event:', err);
-          } else {
-            console.log('Event saved successfully.');
-          }
-        });
-        trimmedEvents.push(trimmedEvent);
-      });
-      res.send(trimmedEvents);
+  jwtClient.authorize(function(err, tokens) {
+    if (err) {
+      console.log(err);
+      return;
     }
+
+    // Make an authorized request to list Drive files.
+    calendar.events.list({ auth: jwtClient, calendarId: 'sashasemail@gmail.com', timeMin: today, timeMax: tomorrow }, function(err, response) {
+      // handle err and response
+      if (err){
+        console.log('Error getting events new style:', err);
+      } else {
+        console.log('Retrieved events from Google.');
+        var events = response.items;
+        var trimmedEvents = [];
+        // loop through and store items in database
+        events.map(function(event){
+          var title = event.summary;
+          var start = event.start.dateTime;
+          var end = event.end.dateTime;
+          var trimmedEvent = {title: title, start: start, end: end};
+          trimmedEvents.push(trimmedEvent);
+          // Event.create(trimmedEvent, function(err){
+          //   if (err){
+          //     console.log('Error saving event:', err);
+          //   } else {
+          //     console.log('Event saved successfully.');
+          //   }
+          // });
+
+        });
+        res.send(trimmedEvents);
+      }
+    });
   });
 
-});
+}); // get
 
 module.exports = router;
-
-
-// for ajax reference
-// ajax('http://omdbapi.com?t=heat', function(error, response, body){
-//   if (error){
-//     console.log('AJAX error', error);
-//   } else {
-//     //console.log('Response', response);
-//     console.log('Body', body);
-//   }
-// });
--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
 // node modules
 var express = require('express');
-var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 var mongoose = require('mongoose');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // custom modules
 var index = require('./routes/index');
-var auth = require('./routes/auth');
 var calendar = require('./routes/calendar');
 var nyt = require('./routes/nyt');
 var apod = require('./routes/apod');
@@ -21,35 +17,7 @@ var app = express();
 // config
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: false,
-  cookie: {maxAge: 60000, secure: false}
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// cors ???
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// authentication strategy for google calendar
 app.use(express.static('server/public'));
-passport.use(new GoogleStrategy({
-  clientID: '565721346198-chm4ukasqlt7djrfsq1v153uf6e3d18n.apps.googleusercontent.com',
-   clientSecret: 'au1FATiwFoolk-ov_tKHj9hm',
-   callbackURL: "http://localhost:3000/auth/callback",
-   scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar']
-  },
-  function(accessToken, refreshToken, profile, done) {
-    profile.accessToken = accessToken;
-   return done(null, profile);
-  }
-));
 
 // database
 var mongoURI = 'mongodb://localhost/dashboard';
@@ -63,10 +31,8 @@ mongoDB.once('open', function(){
   console.log('Connected to database.');
 });
 
-
 // routes
 app.use('/calendar', calendar);
-app.use('/auth', auth);
 app.use('/', index);
 
 
