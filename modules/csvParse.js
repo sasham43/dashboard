f4a68fa0260f4d8a37ddfd5fe0118da610303a2f
sasham43 <sashasemail@gmail.com>
2016-06-05 15:54:58 -0500

    Comment out csvParse

--- modules/csvParse.js
+++ modules/csvParse.js
@@ -3,11 +3,11 @@ var csv = require('csv-parser');
 
 var stopArray = [];
 
-fs.createReadStream('stops.csv').pipe(csv()).on('data', function(data){
-  // console.log('row', data);
-  stopArray.push(data);
-}).on('end', function(){
-  //console.log('StopArray', stopArray);
-});
+// fs.createReadStream('stops.csv').pipe(csv()).on('data', function(data){
+//   // console.log('row', data);
+//   stopArray.push(data);
+// }).on('end', function(){
+//   //console.log('StopArray', stopArray);
+// });
 
 module.exports = stopArray;
